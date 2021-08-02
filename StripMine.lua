fabc248ee70cd3e73bbc60a7f65853787252ba6d
James-H-Cooper <77759957+James-H-Cooper@users.noreply.github.com>
2021-08-02 16:02:31 +0100

    Add files via upload

---
+++ StripMine.lua
@@ -0,0 +1,27 @@
+------------------------------
+-- StripMine.Lua 
+-- By James Cooper
+-- A script to stripmine an area. 
+-- Runs SimpleMine.lua at every depth up to the given argument
+------------------------------
+
+
+-- Get Mining Depth from Arg list
+function init()
+    if #arg==1 then 
+        return tonumber(arg[1])
+    else
+        return 0 
+    end
+end
+
+-- Starting logic/loop -- 
+local depth = init()
+if depth < 1 then
+    print("Invalid parameter, returning early")
+    return false
+end
+for i = 1, depth do
+    print("Running mine at level " .. i)
+    shell.run("SimpleMine ".. i)
+end
